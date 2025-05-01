bd2e077a6c0985bf695d1b787835ef71378268ef
manshu2027 <manshubrat123@gmail.com>
2025-05-01 19:25:10 +0530

    Add files via upload

---
+++ pyhton 4.py
@@ -0,0 +1,9 @@
+// temp
+
+temp= []
+
+for i in range(10);
+     temp = float(input(f"enter temperature {i=1}: "))
+      temp.append(temp)
+     average = sum(temp) / len(temp)
+     print(f"\nAverage temperature: {average:.2f} ")
---
+++ python 1.swift
@@ -0,0 +1,9 @@
+ 
+ // question 1  about me 
+
+ name = input("enter name: ")
+ age = input("enter your age: ")
+location = input(" enter your location: ")
+interest = input("enter your interest: ")
+
+print(f"\nHi, I"m{name}, I"m {age} years old. I live in {location}. i"m really interested in {interest}.)
